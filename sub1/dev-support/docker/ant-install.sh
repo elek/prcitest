a916af4ec026de3a63fed036eda6821561575b67
Márton Elek <elek@apache.org>
2019-01-05 16:40:13 +0100

    rat fix

--- sub1/dev-support/docker/ant-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /opt
-curl -sL 'https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=/ant/binaries/apache-ant-1.10.5-bin.tar.gz' | tar -xz  && \
-   mv apache-ant* /opt/ant
--- sub1/dev-support/docker/findbugs-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /opt && \
-curl -sL https://sourceforge.net/projects/findbugs/files/findbugs/3.0.1/findbugs-3.0.1.tar.gz/download | tar -xz  && \
-   mv findbugs-* /opt/findbugs
